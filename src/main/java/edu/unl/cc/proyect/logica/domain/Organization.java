4fcd71159698055b557c38f67b67a34590162705
Dario Chillogallo <dario.chillogallo@unl.edu.ec>
2026-05-28 17:14:33 -0500

    Modificacion Organization: nuevos metodos y atributos tipo DataTipe

--- src/main/java/edu/unl/cc/proyect/logica/domain/Organization.java
+++ src/main/java/edu/unl/cc/proyect/logica/domain/Organization.java
@@ -1,25 +1,49 @@
 package edu.unl.cc.proyect.logica.domain;
 
+import java.time.Duration;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Organization {
     private String name;
     private String address;
-    private String bussinessHour;
+    private LocalTime oppeningHour;
+    private LocalTime closingHour;
 
+    //Relacion de composicion:
     private List<Field> fields;
 
+    //Constructores
     public Organization(){
         this.fields = new ArrayList<>();
     }
 
-    public Organization(String name, String address, String bussinessHour){
+    public Organization(String name, String address, LocalTime oppeningHour, LocalTime closingHour){
         this.name = name;
         this.address = address;
-        this.bussinessHour = bussinessHour;
+        this.oppeningHour = oppeningHour;
+        this.closingHour = closingHour;
         this.fields = new ArrayList<>();
     }
+
+    //metodos
+    public void addField(Field field){
+        this.fields.add(field);
+    }
+
+    public void deleteField(Field field){
+        if (this.fields.size() <= 1) {
+            throw new IllegalStateException("La Organizacion no se puede quedar sin canchas");
+        }
+        this.fields.remove(field);
+    }
+
+    public Duration calculateBussinessHours(){
+        return Duration.between(oppeningHour, closingHour);
+    }
+
+    // getters and setters
 
     public String getName() {
         return name;
@@ -37,12 +61,20 @@ public class Organization {
         this.address = address;
     }
 
-    public String getBussinessHour() {
-        return bussinessHour;
+    public LocalTime getOppeningHour() {
+        return oppeningHour;
     }
 
-    public void setBussinessHour(String bussinessHour) {
-        this.bussinessHour = bussinessHour;
+    public void setOppeningHour(LocalTime oppeningHour) {
+        this.oppeningHour = oppeningHour;
+    }
+
+    public LocalTime getClosingHour() {
+        return closingHour;
+    }
+
+    public void setClosingHour(LocalTime closingHour) {
+        this.closingHour = closingHour;
     }
 
     public List<Field> getFields() {
