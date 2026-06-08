a7f2f84e66cdb2b81c28f049625b6f3f1710b393
Javier Guarnizo <javierguarnizo11@gmail.com>
2026-06-07 20:57:27 -0500

    Añadiendo un constructor

--- src/main/java/edu/unl/cc/proyect/logica/domain/Reservation.java
+++ src/main/java/edu/unl/cc/proyect/logica/domain/Reservation.java
@@ -21,6 +21,9 @@ public class Reservation implements Serializable {
         this.date = LocalDateTime.now();
     }
 
+    public Reservation(LocalDateTime date, int numberOfPlayers) {
+    }
+
     public boolean validateAvailability() {
         if (this.field == null || this.schedule == null) {
             return false;
